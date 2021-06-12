0d3305182c9175b2021714efcdd0f559606aa639
vegamissyl <63802958+vegamissyl@users.noreply.github.com>
2021-06-12 21:34:06 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from cx_Freeze import setup, Executable
+
+setup(
+      name = "Projet Rothéa Menudier",
+      version = "0.1",
+      description = " porjet IN55 ",
+      executables = [Executable("main.py")]
+      
+      )
+
